737c831a61e97c7023534ada7b9ba2f6d93429cd
NataliaK3108 <124307435+NataliaK3108@users.noreply.github.com>
2023-02-02 18:11:51 -0300

    Add files via upload

---
+++ DESARROLLOWEB/CV/JS/funcionescv.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Sus datos fueron enviados");
+    document.getElementById("contacto").innerHTML = "Muchas gracias por contactarse, me comunicaré a la brevedad"
+}
